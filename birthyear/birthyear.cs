5d1780eb4d9e6475a16eca34b50ff5f1ec4ffb27
avin2020 <77496272+avin2020@users.noreply.github.com>
2021-01-15 19:27:45 +0300

    Add files via upload

---
+++ birthyear/birthyear.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("tamant chanda : ");
+            DateTime aesta = DateTime.Now;
+            int taman = Convert.ToInt32(Console.ReadLine());
+            int birth = aesta.Year - taman;
+            Console.WriteLine("to shali " + birth + " hatuytata dunyawa.");
+            Console.ReadLine();
+        }
+    }
+}
